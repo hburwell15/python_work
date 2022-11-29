f9c076f6a6444e7d81ed42ed284e3c49feb95673
Hannah Burwell <hburwell015@gmail.com>
2022-11-28 21:11:55 -0600

    figured out the solution to a new code problem

--- codewars/fake_binary.py
+++ codewars/fake_binary.py
@@ -1,9 +1,10 @@
 def fake_bin(x):
+    new_string = ""
     for new_binary in x:
-        if new_binary < 5:
-            new_binary.replace(new_binary,"0")
-        if new_binary >= 5:
-            new_binary.replace(new_binary,"1")
-    return new_binary
+        if int(new_binary) < 5:
+            new_string += new_binary.replace(new_binary,"0")
+        if int(new_binary) >= 5:
+            new_string += new_binary.replace(new_binary,"1")
+    return new_string
 
-print(fake_bin("123456789"))
+print(fake_bin("000111199229"))
