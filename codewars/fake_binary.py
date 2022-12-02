cb1865b1a3acabb58503296b147f0025356a1504
Hannah Burwell <hburwell015@gmail.com>
2022-12-01 21:27:09 -0600

    tweaked a code and added a new problem

--- codewars/fake_binary.py
+++ codewars/fake_binary.py
@@ -1,10 +1,10 @@
-def fake_bin(x):
+def fake_bin(old_string):
     new_string = ""
-    for new_binary in x:
-        if int(new_binary) < 5:
-            new_string += new_binary.replace(new_binary,"0")
-        if int(new_binary) >= 5:
-            new_string += new_binary.replace(new_binary,"1")
+    for digit in old_string:
+        if int(digit) < 5:
+            new_string += "0"
+        if int(digit) >= 5:
+            new_string += "1"
     return new_string
 
 print(fake_bin("000111199229"))
