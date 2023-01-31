29e7fa74627948fe9b7cfec4201bb823f4e19cee
Hannah Burwell <hburwell015@gmail.com>
2023-01-30 21:48:56 -0600

    added while loops for keeping a player in the game and added a game over break into the game. Also made a new file that gives random numbers

---
+++ udemy/modules_and_randomization.py
@@ -0,0 +1,8 @@
+import random
+
+random_integer = random.randint(1,10)
+#Will return a random whole number between the spcified numbers in the parenthesis, and will INCLUDE the specified numbers
+print(random_integer)
+
+random_float = random.random(0,5)
+#Will return a floating point number between 0 and 1, but will NOT include 0 and 1. This function also does not take arguments
--- udemy/treasure_island_v1.py
+++ udemy/treasure_island_v1.py
@@ -1,3 +1,5 @@
+game_over = False
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -21,38 +23,60 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
+print("Your mission is to find the treasure.\n")
 
-left_or_right = input("Do you venture Left or Right?\n")
-lowercase_left_or_right = left_or_right.lower()
+while game_over == False:
+  left_or_right = input('You come to a fork in the road. Do you venture left or right? Type "left" or "right"\n').lower().strip()
+ 
+  if left_or_right == "left":
+    while game_over == False:
+      swim_or_wait = input('\nYou walk to the shore. It\'s very foggy out on the water. \nDo you swim or wait? Type "swim" or "wait"\n').lower().strip()
+      if swim_or_wait == "wait":
+        while game_over == False:
+          cat = input('\nA small boat drifts out of the fog to the shore. Out jumps a black cat. He looks at you for a long moment. \nDo you pet or ignore? Type "pet" or "ignore"\n').lower().strip()
+          if cat == "pet":
+            while game_over == False:
+              houses = input('\nThe cat accepts your pets and gives you +1 Luck. \nYou take the boat to the island. There are 3 houses in front of you. \nDo you choose red, blue or yellow? Type "red", "blue", or "yellow"\n').lower().strip()
+              if houses == "yellow":
+                print("You walk into the house to find a large chest sitting in the corner. You open it to see it's full of treasure. \nYOU WIN!")
 
-if lowercase_left_or_right == "left":
-  swim_or_wait = input("\nYou walk to the shore. It's very foggy out on the water. \nDo you Swim or Wait? ")
-  lowercase_swim_or_weight = swim_or_wait.lower()
-  if lowercase_swim_or_weight == "wait":
-    cat = input("\nA small boat drifts out of the fog to the shore. Out jumps a black cat. He looks at you for a long moment. \nDo you Pet or Ignore? ")
-    lowercase_cat = cat.lower()
-    if lowercase_cat == "pet":
-      houses = input("\nThe cat accepts your pets and gives you +1 Luck. \nYou take the boat to the island. There are 3 houses in front of you. \nDo you choose Red, Blue or Yellow? ")
-      lowercase_houses = houses.lower()
-      if lowercase_houses == "yellow":
-        print("You walk into the house to find a large chest sitting in the corner. You open it to see it's full of treasure. \nYOU WIN!")
-      elif lowercase_houses == "blue":
-        print("\nYou see a wizard sitting and having his afternoon tea. He immediatly turns you to ice. He was never one for unwanted visitors. \nGame Over.")
+              elif houses == "blue":
+                game_over = True
+                print("\nYou see a wizard sitting and having his afternoon tea. He immediatly turns you to ice. He was never one for unwanted visitors. \nGame Over.")
+
+              elif houses == "red":
+                game_over = True
+                print("\nYou see you've walked into a corporate business meeting. Everyone glares at you and you die from embaressment. \nGame Over.")
+              else:
+                print("\nI do not recognize this input, please try again")
+
+          elif cat == "ignore":
+            while game_over == False:
+              houses = input('\nYou ignore the cat and get in the boat. The cat flicks its tail at you dismissivle and walks away into the fog. \nYou arrive at the island where there are 3 house. \nDo you choose red, blue or yellow? Type "red", "blue", or "yellow"\n').lower().strip()
+              if houses == "yellow":
+                print("\nYou walk into the house to find a large chest sitting in the corner. You open it to see it's full of treasure. \nYOU WIN!")
+
+              elif houses == "blue":
+                game_over = True
+                print("\nYou see a wizard sitting and having his afternoon tea. He immediatly turns you to ice. He was never one for unwanted visitors. \nGame Over.")
+                
+              elif houses == "red":
+                game_over = True
+                print("\nYou see you've walked into a corporate business meeting. Everyone glares at you and you die from embaressment. \nGame Over.")
+              else:
+                print("\nI do not recognize this input, please try again")
+
+          else:
+            print("\nI do not recognize this input, please try again")  
+      elif swim_or_wait == "swim":
+        game_over = True
+        print("You are sequced by sirens and drown.\nGame Over.")
       else:
-        print("\nYou see you've walked into a corporate business meeting. Everyone glares at you and you die from embaressment. \nGame Over.")
-    else:
-      houses = input("\nYou ignore the cat and get in the boat. The cat flicks its tail at you dismissivle and walks away into the fog. \nYou arrive at the island where there are 3 house. \nDo you choose Red, Blue or Yellow? ")
-      lowercase_houses = houses.lower()
-      if lowercase_houses == "yellow":
-         print("\nYou walk into the house to find a large chest sitting in the corner. You open it to see it's full of treasure. \nYOU WIN!")
-      elif lowercase_houses == "blue":
-        print("\nYou see a wizard sitting and having his afternoon tea. He immediatly turns you to ice. He was never one for unwanted visitors. \nGame Over.")
-      else:
-        print("\nYou see you've walked into a corporate business meeting. Everyone glares at you and you die from embaressment. \nGame Over.")
-      
+        print("\nI do not recognize this input, please try again.")
+          
+  elif left_or_right == "right":
+    game_over = True
+    print("You have fallen into quicksand. \nGame Over.")
   else:
-    print("You attempt to swim across, but are seduced by sirens and drown. \nGame Over.")
-  
-else:
-  print("You have fallen into quicksand. \nGame Over.")
+    print("\nI do not recognize this input, please try again.")
+     
