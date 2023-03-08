0923c8dd9d4aa93ddbd0f5160152838b592b65f4
Hannah Burwell <hburwell015@gmail.com>
2023-03-08 16:25:55 -0600

    for the challenge I did I needed to round the number up to the neareast whole number. I had thought that is hat round did but I was mistaken. I changed to code to import math and use math.ceil to round up the number regardless of its value.

--- udemy/paint_calculation.py
+++ udemy/paint_calculation.py
@@ -1,5 +1,6 @@
+import math
 def paint_calc(height, width, cover):
-   print(f"You'll need {round((test_h * test_w) / coverage)} cans of paint.")
+   print(f"You'll need {math.ceil((test_h * test_w) / coverage)} cans of paint.")
 
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
