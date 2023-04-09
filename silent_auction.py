8fe5561df60cd4914c075a6bced7011f2426086f
Hannah Burwell <hburwell015@gmail.com>
2023-04-08 20:17:58 -0500

    created a silent auction program that will take multiple inputs for peoples bids on an item and calculate who had the highest bid. Also created a file to save the ASKII art in for this program. Created a file for learning how nesting in dictonaries works. Created a days in the month program that calulcates how many days are in each month for that given year. This program is mostly just helpful for knowing when leap years are as February has 29 days on a leap year instead of 28

---
+++ silent_auction.py
@@ -0,0 +1,41 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+from art import logo
+
+print(logo)
+
+print("Welcome to the secret auction program!\n")
+
+auction_bids = {}
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}!")
+
+keep_bidding = True
+while keep_bidding:
+  name = input("What is your name?\n")
+  bid_price = int(input("How much would you like to bid? Please do not add any commas. \n$"))
+  
+  auction_bids[name] = bid_price
+  print(auction_bids)
+
+  more_bidders = input("Is there anyone else who would like to bid? Type 'yes' or 'no'.\n").lower()
+  if more_bidders == "yes":
+    clear()
+  elif more_bidders == "no":
+    keep_bidding = False
+    find_highest_bidder(auction_bids)
+
+
+#Another way to get the highest bidding amount
+# highest_bidder = max(auction_bids, key=auction_bids.get)
+
+# print(f"The winner is {highest_bidder} with a bid of {auction_bids[highest_bidder]}!")
+  
